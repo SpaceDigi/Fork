fa8f29eb88983be18c27f7bcb116231d3c49547a
SpaceDigi <smvk77a44@gmail.com>
2018-02-18 16:17:26 +0200

    95% todo removing

--- js.js
+++ js.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
-    var max_id = 2;
+    var max_id_list = 2;
+    var counter_basket_not_buy = 2;
+    var basket_row_not_buy_id = 0;
+    var counter_basket_buy = 0;
+    var basket_row_buy_id = 0;
+
     document.onkeyup = function (e) {
         e = e || window.event;
         if (e.keyCode === 13) {
@@ -26,13 +31,17 @@ $(document).ready(function () {
         $(".remove").click(function () {
             remove($(this).attr('id'))
         })
+        $(".not-bought").click(function () {
+            returnState($(this).attr('id'))
+        })
     }
 
     function increment(s) {
         arr = s.split("_")
         var index = arr[arr.length - 1]
         var now = $("#counts_" + index).text();
-        $("#counts_" + index).text(Number(now) + 1)
+        $("#counts_" + index).text(Number(now) + 1);
+        $("#buy_counts_" + index).text(Number(now) + 1);
     }
 
     function decrement(s) {
@@ -41,61 +50,159 @@ $(document).ready(function () {
         var now = $("#counts_" + index).text();
         if (Number(now) > 0) {
             $("#counts_" + index).text(Number(now) - 1)
+            $("#buy_counts_" + index).text(Number(now) - 1);
         }
     }
 
     function addingNewProduct(product) {
-        var product_id = "product_"+(++max_id);
-        var delete_id = "delete_"+max_id;
-        var buy_id = "buy_"+max_id;
-        var add_id = "add_"+max_id;
-        var remove_id ="remove_"+max_id;
-        var counts_id = "counts_"+max_id;
-        var label_id = "label_"+max_id;
-        $(".buys").append(' <div class="product" id="'+product_id+'"' +
+        counter_basket_not_buy++;
+        var product_id = "product_" + (++max_id_list);
+        var delete_id = "delete_" + max_id_list;
+        var buy_id = "buy_" + max_id_list;
+        var add_id = "add_" + max_id_list;
+        var remove_id = "remove_" + max_id_list;
+        var counts_id = "counts_" + max_id_list;
+        var label_id = "label_" + max_id_list;
+        var not_buy_id = "notbuy_" + max_id_list;
+        $(".buys").append(' <div class="product" id="' + product_id + '"' +
             '>\n' +
             '                <span class="text-product"' +
-            ' id="' + label_id+'"' + '>' + product +
+            ' id="' + label_id + '"' + '>' + product +
             '</span>\n' +
             '                <div class="controls">\n' +
-            '                    <button class="button delete" id="'+delete_id+'"' + max_id +
+            '                    <button class="button delete" id="' + delete_id + '"' + max_id_list +
             '>-</button>\n' +
-            '                    <span class="counts" id="'+counts_id+'"' + max_id +
+            '                    <span class="counts" id="' + counts_id + '"' + max_id_list +
             '>1</span>\n' +
-            '                    <button class="button add" id="'+add_id+'"' + max_id +
+            '                    <button class="button add" id="' + add_id + '"' + max_id_list +
             '>+</button>\n' +
             '                </div>\n' +
             '                <div class="state">\n' +
             '                    <div class="controls-state">\n' +
-            '                        <button class="bought" id="'+buy_id+'"' +
+            '                        <button class="bought" id="' + buy_id + '"' +
             '>Bought</button>\n' +
-            '                        <button class="remove" id="'+remove_id+'"'+
+            '                        <button class="remove" id="' + remove_id + '"' +
             '>X</button>\n' +
+            ' <button class="not-bought" id="' + not_buy_id + '"' +
+            '>Not bought</button>' +
             '                    </div>\n' +
             '                </div>\n' +
             '            </div>')
-        addListenersNew(max_id)
+        addingNewProductInList(product)
+        addListenersNew(max_id_list)
     }
-    
+
+    function addingNewProductInList(product) {
+        if (counter_basket_not_buy % 3 == 0) {
+            basket_row_not_buy_id++;
+            var section_row_id = "section_row_" + basket_row_not_buy_id;
+            $(".section-rest").append('<div class="section-row" id="' + section_row_id + '"></div>')
+            var section_id = "section_" + max_id_list;
+            var buy_label_id = "buy_label_" + max_id_list;
+            var buy_counts_id = "buy_counts_" + max_id_list;
+            $("#section_row_" + basket_row_not_buy_id).append('<div class="section" id="' + section_id + '"' +
+                '>\n' +
+                '<div class="buy-product" id="' + buy_label_id + '"' +
+                '>' + product +
+                '<span class="counts-bought" id="' + buy_counts_id + '"' +
+                '>1</span></div>\n' +
+                '</div>')
+        }
+        else {
+            var section_id = "section_" + max_id_list;
+            var buy_label_id = "buy_label_" + max_id_list;
+            var buy_counts_id = "buy_counts_" + max_id_list;
+            $("#section_row_" + basket_row_not_buy_id).append('<div class="section" id="' + section_id + '"' +
+                '>\n' +
+                '<div class="buy-product" id="' + buy_label_id + '"' +
+                '>' + product +
+                '<span class="counts-bought" id="' + buy_counts_id + '"' +
+                '>1</span></div>\n' +
+                '</div>')
+        }
+    }
+
     function buy(s) {
         arr = s.split("_")
         var index = arr[arr.length - 1]
-        $("#add_"+index).css("display","none");
-        $("#delete_"+index).css("display","none");
-        $("#label_"+index).css("text-decoration","line-through");
-        $("#remove_"+index).css("display","none");
+        $("#add_" + index).css("display", "none");
+        $("#delete_" + index).css("display", "none");
+        $("#label_" + index).css("text-decoration", "line-through");
+        $("#remove_" + index).css("display", "none");
+        $("#buy_" + index).css("display", "none");
+        $("#notbuy_" + index).css("display", "inline")
+        moveProductToBasket(index);
+    }
+
+    function returnState(s) {
+        arr = s.split("_")
+        var index = arr[arr.length - 1]
+        $("#add_" + index).css("display", "inline");
+        $("#delete_" + index).css("display", "inline");
+        $("#label_" + index).css("text-decoration", "none");
+        $("#remove_" + index).css("display", "inline");
+        $("#buy_" + index).css("display", "inline");
+        $("#notbuy_" + index).css("display", "none")
+        moveProductFromBusket(index)
     }
 
     function remove(s) {
+        counter_basket_not_buy--;
         arr = s.split("_")
         var index = arr[arr.length - 1]
-        $("#product_"+index).remove()
+        $("#product_" + index).remove()
+        $("#section_" + index).remove()
     }
+
     function addListenersNew(id) {
-        $("#remove_"+id).click(function(){remove($(this).attr('id'))})
-        $("#buy_"+id).click(function(){buy(($(this).attr('id')))})
-        $("#add_"+id).click(function(){increment($(this).attr('id'))})
-        $("#delete_"+id).click(function () {decrement($(this).attr('id'))})
+        $("#remove_" + id).click(function () {
+            remove($(this).attr('id'))
+        })
+        $("#buy_" + id).click(function () {
+            buy(($(this).attr('id')))
+        })
+        $("#add_" + id).click(function () {
+            increment($(this).attr('id'))
+        })
+        $("#delete_" + id).click(function () {
+            decrement($(this).attr('id'))
+        })
+        $("#notbuy_" + id).click(function () {
+            returnState($(this).attr('id'))
+        })
+    }
+
+    function moveProductToBasket(index) {
+        var elem = $("#section_" + index).html();
+        if (counter_basket_buy % 3 == 0&&counter_basket_buy>2) {
+            basket_row_buy_id++;
+            var basket_row_id = "basket_row_" + basket_row_buy_id;
+            $(".basket").append('<div class="section-row" id="' + basket_row_id + '"></div>')
+            $("#basket_row_" + basket_row_buy_id).append(elem);
+        }
+        else {
+            $("#basket_row_" + basket_row_buy_id).append(elem);
+        }
+        $("#section_" + index).remove();
+        counter_basket_buy++;
+        counter_basket_not_buy--;
+    }
+
+    function moveProductFromBusket(index) {
+        var elem = $("#basket_row_" + index).html();
+        //TODO
+        /*if (counter_basket_not_buy % 3 == 0) {
+            var section_row_id = "section_row_" + basket_row_not_buy_id;
+            $("#section_row_"+basket_row_not_buy_id).append(elem)
+            $(".section-rest").append('<div class="section-row" id="' + section_row_id + '"></div>')
+            basket_row_not_buy_id++;
+        }
+        else{
+            $("#section_row_" + basket_row_not_buy_id).append(elem);
+        }
+        counter_basket_not_buy++;
+        counter_basket_buy--;*/
+        $("#basket_row_" + index).remove();
     }
 
 });
